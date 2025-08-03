c076e3207aa59419d74390d1a3020586fb58cf6f
Joe Bell <35937399+llebj@users.noreply.github.com>
2025-08-03 15:27:53 +0100

    Configured application insights logging in Azure functions.

--- BackEndProcessing/Program.cs
+++ BackEndProcessing/Program.cs
@@ -1,14 +1,25 @@
+using System.Reflection;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
+builder.Configuration
+    .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 
 builder.Build().Run();
